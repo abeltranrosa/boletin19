17baedc918be05c97b12c0ccd601ac255399b7a9
antonio <antonio@biruseto>
2017-02-10 11:29:53 +0100

    clase libreria boletin 19

--- boletin19/src/boletin19/Libreria.java
+++ boletin19/src/boletin19/Libreria.java
@@ -7,54 +7,53 @@ import javax.swing.JOptionPane;
 import java.util.Collections;
 import static beltran.Beltran.pedirString;
 
-public class Libreria {
+public class Libreria { 
 
-    ArrayList<Libro> Libreria = new ArrayList<>();
+    ArrayList<Libro> librerias = new ArrayList<Libro>();
 
     public void cargarArray() {
          Libro libro1 = new Libro("El codigo da Vincci","Dan Brown","z666",45,10);
          Libro libro2 = new Libro("El cruasan","Martin Lutero","z652",15,25);
          Libro libro3 = new Libro("La biblia","Desconocido","z125", 25,50);
         
-         Libreria.add(libro1);
-         Libreria.add(libro2);
-         Libreria.add(libro3);
+         librerias.add(libro1);
+         librerias.add(libro2);
+         librerias.add(libro3);
          
     }
 
     public void añadirLibros() {
-        Libreria.add(new Libro(pedirString(), pedirString(), pedirString(), pedirFloat(), pedirInt()));
+        librerias.add(new Libro(pedirString(), pedirString(), pedirString(), pedirFloat(), pedirInt()));
     }
 
     public void venderLibros(int x) {
-        Libreria.remove(x);
+        librerias.remove(x);
     }
 
     public void AmosarLibros() {
-        Collections.sort(Libreria, null);
+        //Collections.sort(Libreria, null);
         String cadena = " ";
-        for (int i = 0; i < Libreria.size(); i++) {
-            if (libro[i] = libro[i]) {
-                cadena += libro[i] + " " + libro[i] + "\n";
-            }
+        for (int i = 0; i < librerias.size(); i++) {
+            JOptionPane.showMessageDialog(null, librerias.get(i));
+        }
 
-            return cadena;
+            
         }
    
 
     public void Dardebajalibros() {
-        for (int i = 0; i < Libreria.size(); i++) {
-            if (Libreria.get(i).getNumerounidades() == 0) {
-                Libreria.remove(i);
+        for (int i = 0; i < librerias.size(); i++) {
+            if (librerias.get(i).getNumerounidades() == 0) {
+                librerias.remove(i);
             }
         }
     }
 
     public void ConsultarLibro() {
         String libro = JOptionPane.showInputDialog("Introduce el titulo del libro");
-        for (int i = 0; i < Libreria.size(); i++) {
-            if (Libreria.get(i).getTitulo().equalsIgnoreCase(libro)) {
-                JOptionPane.showMessageDialog(null, Libreria.get(i).toString());
+        for (int i = 0; i < librerias.size(); i++) {
+            if (librerias.get(i).getTitulo().equalsIgnoreCase(libro)) {
+                JOptionPane.showMessageDialog(null, librerias.get(i).toString());
             }
         }
 
