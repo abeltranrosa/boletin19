73ae9c802c94215f9be11780395d43ebb14bcced
antonio <antonio@biruseto>
2017-02-10 11:53:41 +0100

    main boletin 19

--- boletin19/src/boletin19/Boletin19.java
+++ boletin19/src/boletin19/Boletin19.java
@@ -9,7 +9,7 @@ public class Boletin19 {
         int opcion;
         Libreria menu = new Libreria();
 
-        
+        menu.cargarArray();
 
         do {
             opcion = Integer.parseInt(JOptionPane.showInputDialog("Seleccione una opcion del menu \n 1.Añadir libro \n 2.Vender libro \n 3. Amosar libro \n 4. Dar de baja un libro \n 5. Consulta un libro"));
@@ -17,13 +17,13 @@ public class Boletin19 {
                 case 1:
                     menu.añadirLibros();
                 case 2:
-                    menu.venderLibros(JOptionPane.showInputDialog("Indica el libro que deseas vender :"));
+                    menu.venderLibros();
                     break;
                 case 3:
-                    JOptionPane.showMessageDialog(null, menu.AmosarLibros());
+                    menu.AmosarLibros();
                     break;
                 case 4:
-                    menu.DardebajaLibros();
+                    menu.Dardebajalibros();
                     break;
                 case 5:
                     menu.ConsultarLibro();
@@ -36,4 +36,5 @@ public class Boletin19 {
         } while (opcion<6);
 
     }
+    
 }
