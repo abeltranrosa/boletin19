f84cb80010a2effcf36e79540596c62f2e56cc82
antonio <antonio@biruseto>
2017-02-10 11:48:57 +0100

    clase libreria boletin 19

--- boletin19/src/boletin19/Libreria.java
+++ boletin19/src/boletin19/Libreria.java
@@ -12,7 +12,7 @@ public class Libreria {
     ArrayList<Libro> librerias = new ArrayList<Libro>();
 
     public void cargarArray() {
-         Libro libro1 = new Libro("El codigo da Vincci","Dan Brown","z666",45,10);
+         Libro libro1 = new Libro("El codigo da Vinci","Dan Brown","z666",45,10);
          Libro libro2 = new Libro("El cruasan","Martin Lutero","z652",15,25);
          Libro libro3 = new Libro("La biblia","Desconocido","z125", 25,50);
         
@@ -26,8 +26,17 @@ public class Libreria {
         librerias.add(new Libro(pedirString(), pedirString(), pedirString(), pedirFloat(), pedirInt()));
     }
 
-    public void venderLibros(int x) {
-        librerias.remove(x);
+    public void venderLibros() {
+        Libro ventaLibro = new Libro(pedirString(), pedirString(), pedirString(), pedirFloat(), pedirInt());
+        for (int i=0;i<librerias.size();i++){
+            if(ventaLibro.getISBN().equals(librerias.get(i).getISBN())){
+                if (librerias.get(i).getNumerounidades()>=ventaLibro.getNumerounidades()){
+                    librerias.get(i).setNumerounidades(librerias.get(i).getNumerounidades()-ventaLibro.getNumerounidades());
+                }
+            }else
+                System.out.println("No hay suficientes unidades");
+        }
+       
     }
 
     public void AmosarLibros() {
@@ -52,8 +61,10 @@ public class Libreria {
     public void ConsultarLibro() {
         String libro = JOptionPane.showInputDialog("Introduce el titulo del libro");
         for (int i = 0; i < librerias.size(); i++) {
-            if (librerias.get(i).getTitulo().equalsIgnoreCase(libro)) {
-                JOptionPane.showMessageDialog(null, librerias.get(i).toString());
+            if (librerias.get(i).getTitulo().equals(libro)) {
+                //JOptionPane.showMessageDialog(null, librerias.get(i).toString());
+                System.out.println("El libro esta disponible");
+                
             }
         }
 
